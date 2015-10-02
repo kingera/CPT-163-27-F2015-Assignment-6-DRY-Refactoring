69b6e19552c190cc8fd21a1dd107026bb6f02960
Paul Scarrone <ninjapanzer@gmail.com>
2015-10-02 08:55:18 -0400

    Assignment 6 - DRY Refactoring Project Base

---
+++ src/howdryiam/LabelPrinter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package howdryiam;
+
+/**
+ *
+ * @author samuraipanzer
+ */
+public class LabelPrinter {
+  private Address address = null;
+  private Dimensions dimension = null;
+
+  public LabelPrinter(Address address, Dimensions dimension) {
+	this.address = address;
+	this.dimension = dimension;
+  }
+  
+  public void printLabel(){
+	String depth = this.dimension.depthWithUnits();
+	String width = this.dimension.widthWithUnits();
+	String height = this.dimension.heightWithUnits();
+	System.out.println("Name: " + this.address.getAttn() +
+					   "Street: " + this.address.getStreet() +
+					   "City: " + this.address.getCity() +
+					   "State: " + this.address.getState() +
+					   "Zip: " + this.address.getZip() +
+					   "Dimensions: " + depth + "x" + width + "x" + height);
+  }
+}
