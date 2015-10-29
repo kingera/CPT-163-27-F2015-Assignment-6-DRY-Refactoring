767b14215533bbd03e9781b8a1fe94f3407b483c
kingera <kingera1818@gmail.com>
2015-10-28 21:17:40 -0400

    hj

---
+++ extend/src/extend/foo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extend.manoman;
+
+/**
+ *
+ * @author Adam
+ */
+public class foo {
+  public void sayHey(){
+      System.out.println("Hey");
+  }
+  
+  protected void goodNight(){
+      System.out.println("Good Night");
+  }
+  
+  private void nope(){
+      
+  }
+}
